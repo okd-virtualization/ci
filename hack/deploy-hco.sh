66ef8ad7659b755ce9590c5bb213f0bbc1d0107d
Fabian Deutsch <fabiand@fedoraproject.org>
2021-11-09 09:43:16 +0100

    Move the operator subscriptions into it's own file
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- hack/deploy-hco.sh
+++ hack/deploy-hco.sh
@@ -19,53 +19,9 @@ trap "cleanup" INT TERM EXIT
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-echo "creating catalogsource"
-cat <<EOF | oc apply -f -
-apiVersion: operators.coreos.com/v1alpha1
-kind: CatalogSource
-metadata:
-  name: hco-unstable-catalog-source
-  namespace: openshift-marketplace
-spec:
-  sourceType: grpc
-  image: quay.io/kubevirt/hyperconverged-cluster-index:${HCO_RELEASE}-unstable
-  displayName: Kubevirt Hyperconverged Cluster Operator
-  publisher: Kubevirt Project
-EOF
-
-echo "creating namespace"
-cat <<EOF | oc apply -f -
-apiVersion: v1
-kind: Namespace
-metadata:
-    name: ${TARGET_NAMESPACE}
-EOF
-
-echo "creating operator group"
-cat <<EOF | oc apply -f -
-apiVersion: operators.coreos.com/v1
-kind: OperatorGroup
-metadata:
-    name: kubevirt-hyperconverged-group
-    namespace: ${TARGET_NAMESPACE}
-EOF
-
-echo "creating subscription"
-oc create -f - <<EOF
-apiVersion: operators.coreos.com/v1alpha1
-kind: Subscription
-metadata:
-  name: kubevirt-hyperconverged
-  namespace: "${TARGET_NAMESPACE}"
-  labels:
-    operators.coreos.com/kubevirt-hyperconverged.kubevirt-hyperconverged: ''
-spec:
-  channel: ${HCO_RELEASE}
-  installPlanApproval: Automatic
-  name: community-kubevirt-hyperconverged
-  source: hco-unstable-catalog-source
-  sourceNamespace: openshift-marketplace
-EOF
+echo "creating catalogsource, operator group, and subscription"
+oc create namespace ${TARGET_NAMESPACE}
+oc apply -n ${TARGET_NAMESPACE} -f ${SCRIPT_DIR}/../manifests/virtualization.yaml
 
 echo "waiting for HyperConverged operator to become ready"
 "$SCRIPT_DIR"/wait-for-hco.sh
