748beeec9885e65a3376b8a69449fedcf62d5335
Simone Tiraboschi <stirabos@redhat.com>
2021-11-23 09:55:03 +0100

    By default deploy the stable version of HCO
    Signed-off-by: Simone Tiraboschi <stirabos@redhat.com>

--- hack/deploy-hco.sh
+++ hack/deploy-hco.sh
@@ -2,8 +2,9 @@
 
 set -euxo pipefail
 
-HCO_RELEASE=${HCO_RELEASE:-1.6.0}
 TARGET_NAMESPACE=${TARGET_NAMESPACE:-kubevirt-hyperconverged}
+HCO_UNSTABLE=${HCO_UNSTABLE:-false}
+HCO_MANIFESTS_SUFFIX=""
 
 function cleanup() {
     rv=$?
@@ -19,10 +20,14 @@ trap "cleanup" INT TERM EXIT
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+
 echo "creating catalogsource, operator group, and subscription"
 oc create namespace ${TARGET_NAMESPACE}
-oc apply -f ${SCRIPT_DIR}/../manifests/catalog-unstable.yaml
-oc apply -n ${TARGET_NAMESPACE} -f ${SCRIPT_DIR}/../manifests/virtualization.yaml
+if [ "$HCO_UNSTABLE" = "true" ]; then
+    HCO_MANIFESTS_SUFFIX="-unstable"
+    oc apply -f ${SCRIPT_DIR}/../manifests/catalog${HCO_MANIFESTS_SUFFIX}.yaml
+fi
+oc apply -n ${TARGET_NAMESPACE} -f ${SCRIPT_DIR}/../manifests/virtualization${HCO_MANIFESTS_SUFFIX}.yaml
 
 echo "waiting for HyperConverged operator to become ready"
 "$SCRIPT_DIR"/wait-for-hco.sh
